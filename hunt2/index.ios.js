e5b812df3dd00e85591ad89935f174a01f0314b3
Erin Bond <erin@erinebond.com>
2017-01-11 13:50:27 -0800

    Fresh react-native app

---
+++ hunt2/index.ios.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+  TouchableOpacity,
+} from 'react-native';
+
+
+import snapengerHunt from './app/index'
+
+'use strict';
+
+AppRegistry.registerComponent('hunt2', () => hunt2);
